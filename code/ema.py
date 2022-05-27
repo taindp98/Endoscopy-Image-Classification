fdb628bd77375a80e9167b4a6a9898482a5c1d52
Tai NGUYEN <taindp98@gmail.com>
2022-05-27 11:13:05 +0200

    fixbug

--- code/ema.py
+++ code/ema.py
@@ -41,16 +41,16 @@ class ModelEMA(object):
     def __init__(self, model, decay=0.9999, device=None):
         super(ModelEMA, self).__init__()
         # make a copy of the model for accumulating moving average of weights
-        self.module = deepcopy(model)
-        self.module.eval()
+        self.ema = deepcopy(model)
+        self.ema.eval()
         self.decay = decay
         self.device = device  # perform ema on different device from model if set
         if self.device is not None:
-            self.module.to(device=device)
+            self.ema.to(device=device)
 
     def _update(self, model, update_fn):
         with torch.no_grad():
-            for ema_v, model_v in zip(self.module.state_dict().values(), model.state_dict().values()):
+            for ema_v, model_v in zip(self.ema.state_dict().values(), model.state_dict().values()):
                 if self.device is not None:
                     model_v = model_v.to(device=self.device)
                 ema_v.copy_(update_fn(ema_v, model_v))
