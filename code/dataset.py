cf12e06eeb3a381de7466d562fe92485ed8232c1
Tai NGUYEN <taindp98@gmail.com>
2022-06-10 09:11:05 +0200

    add-oc

--- code/dataset.py
+++ code/dataset.py
@@ -50,7 +50,9 @@ def get_transform(config, is_train = False, is_labeled = True):
         if is_labeled:
             trf_aug = transforms.Compose([
                 # transforms.CenterCrop(config.DATA.IMG_SIZE),
-                transforms.Resize((config.DATA.IMG_SIZE,config.DATA.IMG_SIZE)),
+                # transforms.Resize((config.DATA.IMG_SIZE,config.DATA.IMG_SIZE)),
+                transforms.Resize((156,156)),
+                transforms.CenterCrop(config.DATA.IMG_SIZE),
                 transforms.RandomHorizontalFlip(p=0.3),
                 transforms.RandomVerticalFlip(p=0.3),
                 # transforms.RandomRotation(20),
@@ -120,78 +122,96 @@ class GIDataset(torch.utils.data.Dataset):
         #     y = torch.tensor(vec, dtype=torch.long)
             return x, y
 
-def get_data(config, df_anno, df_unanno = None, is_visual=False):
+def get_data(config, df_anno, df_unanno = None, is_pathology = False,is_visual=False):
     """
     get training, validation, testing set
     """
     df_train = df_anno[df_anno['is_valid']==False]
     df_valid = df_anno[df_anno['is_valid']==True]
     ## break down into labeled and unlabeled set
-    if config.TRAIN.IS_SSL:
-        if config.DATA.MOCKUP_SSL:
-            df_labeled = df_train[df_train['is_labeled']==True]
-            df_unlabeled = df_train[df_train['is_labeled']==False]
-            # df_labeled, df_unlabeled = train_test_split(df_train, test_size = config.DATA.MOCKUP_SIZE, random_state = 0)
-            train_labeled_ds = GIDataset(df = df_labeled, config = config, transforms = get_transform(config, is_train=True))
-            train_unlabeled_ds = GIDataset(df = df_unlabeled, config = config, transforms = get_transform(config, is_train=True, is_labeled=False), is_unanno = True)
-            train_labeled_dl = DataLoader(train_labeled_ds, 
-                                        sampler=RandomSampler(train_labeled_ds),
-                                        batch_size = config.DATA.BATCH_SIZE, 
-                                        num_workers = config.DATA.NUM_WORKERS)
+    if is_pathology:
+        if config.TRAIN.IS_SSL:
+            if config.DATA.MOCKUP_SSL:
+                df_labeled = df_train[df_train['is_labeled']==True]
+                df_unlabeled = df_train[df_train['is_labeled']==False]
+                # df_labeled, df_unlabeled = train_test_split(df_train, test_size = config.DATA.MOCKUP_SIZE, random_state = 0)
+                train_labeled_ds = GIDataset(df = df_labeled, config = config, transforms = get_transform(config, is_train=True))
+                train_unlabeled_ds = GIDataset(df = df_unlabeled, config = config, transforms = get_transform(config, is_train=True, is_labeled=False), is_unanno = True)
+                train_labeled_dl = DataLoader(train_labeled_ds, 
+                                            sampler=RandomSampler(train_labeled_ds),
+                                            batch_size = config.DATA.BATCH_SIZE, 
+                                            num_workers = config.DATA.NUM_WORKERS)
 
-            train_unlabeled_dl = DataLoader(train_unlabeled_ds, 
-                                        sampler=RandomSampler(train_unlabeled_ds),
-                                        batch_size = config.DATA.BATCH_SIZE*config.DATA.MU, 
-                                        num_workers = config.DATA.NUM_WORKERS)
-            train_dl = (train_labeled_dl, train_unlabeled_dl)
+                train_unlabeled_dl = DataLoader(train_unlabeled_ds, 
+                                            sampler=RandomSampler(train_unlabeled_ds),
+                                            batch_size = config.DATA.BATCH_SIZE*config.DATA.MU, 
+                                            num_workers = config.DATA.NUM_WORKERS)
+                train_dl = (train_labeled_dl, train_unlabeled_dl)
 
-            if is_visual:
-                for x1, y1 in train_labeled_dl:
-                    # print(x.shape)
-                    # print(y)
-                    break
-                for x2 in train_unlabeled_dl:
-                    break
-                show_grid([x1[0,:,:], x2[0][0,:,:], x2[1][0,:,:]])
-        ## else for real unlabeled data
+                if is_visual:
+                    for x1, y1 in train_labeled_dl:
+                        # print(x.shape)
+                        # print(y)
+                        break
+                    for x2 in train_unlabeled_dl:
+                        break
+                    show_grid([x1[0,:,:], x2[0][0,:,:], x2[1][0,:,:]])
+            ## else for real unlabeled data
+            else:
+                train_labeled_ds = GIDataset(df = df_train, config = config, transforms = get_transform(config, is_train=True))
+                train_unlabeled_ds = GIDataset(df = df_unanno, config = config, transforms = get_transform(config, is_train=True, is_labeled=False), is_unanno = True)
+                train_labeled_dl = DataLoader(train_labeled_ds, 
+                                            sampler=RandomSampler(train_labeled_ds),
+                                            batch_size = config.DATA.BATCH_SIZE, 
+                                            num_workers = config.DATA.NUM_WORKERS)
+
+                train_unlabeled_dl = DataLoader(train_unlabeled_ds, 
+                                            sampler=RandomSampler(train_unlabeled_ds),
+                                            batch_size = config.DATA.BATCH_SIZE*config.DATA.MU, 
+                                            num_workers = config.DATA.NUM_WORKERS)
+                train_dl = (train_labeled_dl, train_unlabeled_dl)
+                if is_visual:
+                    for x1, y1 in train_labeled_dl:
+                        # print(x.shape)
+                        # print(y)
+                        break
+                    for x2 in train_unlabeled_dl:
+                        break
+                    show_grid([x1[0,:,:], x2[0][0,:,:], x2[1][0,:,:]])
         else:
-            train_labeled_ds = GIDataset(df = df_train, config = config, transforms = get_transform(config, is_train=True))
-            train_unlabeled_ds = GIDataset(df = df_unanno, config = config, transforms = get_transform(config, is_train=True, is_labeled=False), is_unanno = True)
-            train_labeled_dl = DataLoader(train_labeled_ds, 
-                                        sampler=RandomSampler(train_labeled_ds),
-                                        batch_size = config.DATA.BATCH_SIZE, 
-                                        num_workers = config.DATA.NUM_WORKERS)
-
-            train_unlabeled_dl = DataLoader(train_unlabeled_ds, 
-                                        sampler=RandomSampler(train_unlabeled_ds),
-                                        batch_size = config.DATA.BATCH_SIZE*config.DATA.MU, 
-                                        num_workers = config.DATA.NUM_WORKERS)
-            train_dl = (train_labeled_dl, train_unlabeled_dl)
+            train_ds = GIDataset(df_train[df_train['is_labeled']==True], config = config, transforms = get_transform(config, is_train=True))
+            train_dl = DataLoader(train_ds, 
+                            sampler=RandomSampler(train_ds),
+                            batch_size = config.DATA.BATCH_SIZE, 
+                            num_workers = config.DATA.NUM_WORKERS)
             if is_visual:
-                for x1, y1 in train_labeled_dl:
+                for x, y in train_dl:
                     # print(x.shape)
                     # print(y)
                     break
-                for x2 in train_unlabeled_dl:
-                    break
-                show_grid([x1[0,:,:], x2[0][0,:,:], x2[1][0,:,:]])
+                show_batch(x[0,:,:], y[0])
+        
+        valid_ds = GIDataset(df_valid , config = config, transforms = get_transform(config))
+        valid_dl = DataLoader(valid_ds, 
+                            sampler=SequentialSampler(valid_ds),
+                            batch_size = config.DATA.BATCH_SIZE, 
+                            num_workers = config.DATA.NUM_WORKERS)
+
     else:
-        df_train_labeled = df_train[df_train['is_labeled']==True]
-        train_ds = GIDataset(df_train_labeled, config = config, transforms = get_transform(config, is_train=True))
+        train_ds = GIDataset(df_train, config = config, transforms = get_transform(config, is_train=True))
         train_dl = DataLoader(train_ds, 
                         sampler=RandomSampler(train_ds),
                         batch_size = config.DATA.BATCH_SIZE, 
                         num_workers = config.DATA.NUM_WORKERS)
         if is_visual:
             for x, y in train_dl:
-                # print(x.shape)
-                # print(y)
                 break
             show_batch(x[0,:,:], y[0])
-    valid_ds = GIDataset(df_valid , config = config, transforms = get_transform(config))
-    valid_dl = DataLoader(valid_ds, 
-                        sampler=SequentialSampler(valid_ds),
-                        batch_size = config.DATA.BATCH_SIZE, 
-                        num_workers = config.DATA.NUM_WORKERS)
 
+        valid_ds = GIDataset(df_valid , config = config, transforms = get_transform(config))
+        valid_dl = DataLoader(valid_ds, 
+                            sampler=SequentialSampler(valid_ds),
+                            batch_size = config.DATA.BATCH_SIZE, 
+                            num_workers = config.DATA.NUM_WORKERS)
+        
     return train_dl, valid_dl
