6d07032bd30dadffd96d963700edb04c57d4e7c7
Tai NGUYEN <taindp98@gmail.com>
2022-07-01 15:53:32 +0200

    fix-name

--- code/semiformer.py
+++ code/semiformer.py
@@ -35,7 +35,7 @@ class SemiFormer:
     def get_config(self, config):
         self.config = config
         # self.config.TRAIN.EVAL_STEP = len(self.train_unlabeled_dl)
-        print('Training mode: FixMatch')
+        print('Training mode: SemiFormer')
         if self.config.TRAIN.USE_EMA:
             self.ema_model = ModelEMA(model = self.model, decay = self.config.TRAIN.EMA_DECAY, device = self.device)
 
