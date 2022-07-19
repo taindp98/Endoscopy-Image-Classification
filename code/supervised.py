29c5851551caca31eb2b621932c75e8420bce563
Tai NGUYEN <taindp98@gmail.com>
2022-07-19 20:57:28 +0200

    focal-no-triplet

--- code/supervised.py
+++ code/supervised.py
@@ -115,7 +115,7 @@ class SupLearning:
                     losses = ce_loss(logits = outputs, 
                                     targets = targets, 
                                     class_weights = self.class_weights, 
-                                    reduction = 'mean', type_loss = 'poly')
+                                    reduction = 'mean', type_loss = 'focal')
             
             self.optimizer.zero_grad()
 
